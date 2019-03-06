b741e9730cc0ccb50ebcd6624a0a217a7bed02c1
Doppeey <lukas.doppler@yahoo.de>
2019-03-06 17:08:47 +0100

    Update to Pm reports
    made the report async, also made it so you supply username+descriminator instead of ID, eliminating the need of users having dev mode enabled

--- EventsAndCommands/TestCommandsAndEvents/PmReportTest.java
+++ EventsAndCommands/TestCommandsAndEvents/PmReportTest.java
@@ -1,14 +1,18 @@
 package EventsAndCommands.TestCommandsAndEvents;
 
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 public class PmReportTest extends ListenerAdapter {
 
-    EventWaiter waiter;
+    private EventWaiter waiter;
 
 
     public PmReportTest(EventWaiter waiter) {
@@ -18,50 +22,90 @@ public class PmReportTest extends ListenerAdapter {
 
     @Override
     public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
-        Runnable timeOut = new Runnable() {
+        Runnable timeOut = () -> event.getChannel().sendMessage("Your report has timed out because you haven't replied in 5 minutes").queue();
+
+        Runnable reportRunnable = new Runnable() {
+
+            private final Member[] reportedMember = new Member[1];
+
             @Override
             public void run() {
-                event.getChannel().sendMessage("Your report has timed out because you haven't replied in 5 minutes").queue();
+                final String[] reportReason = new String[1];
 
+
+                if(event.getMessage().getContentRaw().equalsIgnoreCase("report")){
+                    event.getAuthor().openPrivateChannel().complete().sendMessage("Please supply the name and discriminator of the person you'd like to report \nExample: \"JungleMan#1325\"").queue();
+
+
+                    waiter.waitForEvent(event.getClass(), who -> event.getAuthor().equals(who.getAuthor()), message -> {
+
+                        String messageContent = message.getMessage().getContentRaw();
+                        final Guild togetherJavaGuild = event.getJDA().getGuildById("272761734820003841");
+                        togetherJavaGuild.getMembersByName(messageContent.split("#")[0],true)
+                                .stream()
+                                .filter(member -> member.getUser().getDiscriminator().equalsIgnoreCase(messageContent.split("#")[1]))
+                                .forEach(correctMember -> reportedMember[0] = correctMember);
+
+                        if(reportedMember[0] == null){
+                            event.getChannel().sendMessage("Sorry, can't find that user, please contact a moderator for further assistance.").queue();
+                            return;
+                        }
+
+                        event.getChannel().sendMessage("What did they do ?").queue();
+
+                        waiter.waitForEvent(event.getClass(), why -> event.getAuthor().equals(why.getAuthor()), why -> {
+
+                            reportReason[0] = why.getMessage().getContentRaw();
+                            event.getChannel().sendMessage("Please upload evidence in the form of a screenshot, or type anything to report without evidence").queue();
+
+
+                            waiter.waitForEvent(event.getClass(), evidence -> event.getAuthor().equals(evidence.getAuthor()), evidenceMessage -> {
+
+
+                                EmbedBuilder embedBuilder = new EmbedBuilder();
+                                embedBuilder.setTitle("Report reason");
+                                embedBuilder.setDescription(reportReason[0]);
+                                embedBuilder.addField("Report by",event.getAuthor().getAsMention(),true);
+                                embedBuilder.addField("Reported user", togetherJavaGuild.getMemberById(reportedMember[0].getUser().getId()).getAsMention(),true);
+                                embedBuilder.setThumbnail(reportedMember[0].getUser().getAvatarUrl());
+                                embedBuilder.setColor(Color.red);
+
+                                if(!evidenceMessage.getMessage().getAttachments().isEmpty()) {
+                                    if(evidenceMessage.getMessage().getAttachments().get(0).isImage()) {
+                                        embedBuilder.setImage(evidenceMessage.getMessage().getAttachments().get(0).getUrl());
+                                    }
+                                }
+
+                                togetherJavaGuild.getTextChannelById("430845941461811201").sendMessage(embedBuilder.build()).queue();
+                                why.getChannel().sendMessage("Your report has been submitted and will be reviewed by one of our moderators as soon as possible, thank you!").queue();
+
+
+
+
+                            },5,TimeUnit.MINUTES,timeOut);
+
+
+                        },5,TimeUnit.MINUTES,timeOut);
+
+
+                    }, 5, TimeUnit.MINUTES, timeOut);
+
+
+
+
+
+
+
+
+
+
+                }
             }
         };
-
-        final String[] reportedUser = new String[1];
-        final String[] reportReason = new String[1];
-
-
-        if(event.getMessage().getContentRaw().equalsIgnoreCase("report")){
-            event.getAuthor().openPrivateChannel().complete().sendMessage("Please supply the ID of the person you'd like to report").queue();
+        Thread reportThread = new Thread(reportRunnable);
+        reportThread.start();
 
 
-          waiter.waitForEvent(event.getClass(), who -> event.getAuthor().equals(who.getAuthor()), message -> {
-              System.out.println("yes");
-
-              reportedUser[0] = message.getMessage().getContentRaw();
-              event.getChannel().sendMessage("What did they do ?").queue();
-
-              waiter.waitForEvent(event.getClass(), why -> event.getAuthor().equals(why.getAuthor()), why -> {
-
-                  reportReason[0] = why.getMessage().getContentRaw();
-                  why.getChannel().sendMessage("Your report has been submitted and will be reviewed by one of our moderators as soon as possible, thank you!").queue();
-                  event.getJDA().getGuildById("459848257867350017").getTextChannelById("459848258299232277").sendMessage("User "+event.getAuthor().getName()+" has reported the user with id "+reportedUser[0]+"" +
-                          "\n**Reason:** "+reportReason[0]).queue();
-
-              },5,TimeUnit.MINUTES,timeOut);
-
-
-          }, 5, TimeUnit.MINUTES, timeOut);
-
-
-
-
-
-
-
-
-
-
-        }
 
 
 
