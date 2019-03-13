fcf65a865889d31f91aabd8780bdd99711a6ccdc
Doppeey <25537078+Doppeey@users.noreply.github.com>
2019-03-13 11:12:50 +0100

    Fixed the translate command(it was not sending the requests correctly) by specifying the user agent.
    Credits to I Al Istannen on the Together Java Discord

--- src/main/java/EventsAndCommands/UtilityCommands/TranslateCommand.java
+++ src/main/java/EventsAndCommands/UtilityCommands/TranslateCommand.java
@@ -7,12 +7,17 @@ import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.params.HttpProtocolParamBean;
 
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.nio.charset.StandardCharsets;
 
 public class TranslateCommand extends Command {
 
@@ -23,20 +28,19 @@ public class TranslateCommand extends Command {
 
     @Override
     protected void execute(CommandEvent event) {
-
         String args = event.getArgs();
             String returnString = "";
 
             try {
-                String textEncoded= URLEncoder.encode(args, "utf-8");
+                String textEncoded= URLEncoder.encode(args, StandardCharsets.UTF_8);
                 String url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" + textEncoded;
-                var httpclient = new DefaultHttpClient();
-                CloseableHttpResponse response = httpclient.execute(new HttpGet(url));
+                CloseableHttpClient instance = HttpClients.custom().setUserAgent("Mozilla/5.0 Firefox/26.0").build();
+                CloseableHttpResponse response = instance.execute(new HttpGet(url));
                 StatusLine statusLine = response.getStatusLine();
                 if(statusLine.getStatusCode() == HttpStatus.SC_OK){
                     ByteArrayOutputStream out = new ByteArrayOutputStream();
                     response.getEntity().writeTo(out);
-                    String responseString = out.toString();
+                    String responseString = out.toString(StandardCharsets.UTF_8);
                     out.close();
 
                     String aJsonString = responseString;
