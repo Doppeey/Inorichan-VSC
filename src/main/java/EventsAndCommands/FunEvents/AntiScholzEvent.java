5efa565ba89971beab7c855dd1e55d8bcecf8a8a
RicardoMonteiroSimoes <monteric@students.zhaw.ch>
2019-05-06 22:48:39 +0200

    Update AntiScholzEvent.java
    Fixed the NPE Bug. Kept the Try catch just in case
    Added some modularity for the filter, and also started working on a function to enable it for single users.

--- src/main/java/EventsAndCommands/FunEvents/AntiScholzEvent.java
+++ src/main/java/EventsAndCommands/FunEvents/AntiScholzEvent.java
@@ -3,42 +3,60 @@ package EventsAndCommands.FunEvents;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class AntiScholzEvent extends ListenerAdapter {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
-    private String scholzID = "431542303346262039";
-    private String[] bannedWords = { "opop", "xd", "lmao", "lol", "rofl", "eksd", "dx" };
+public class UserFilteringAdapter extends ListenerAdapter {
+
+    private HashMap<String, Boolean> userIDs;
+    private HashSet<String> bannedWords;
+
+    private final String FILTER_ON = ">spamfilter on";
+    private final String FILTER_OFF = ">spamfilter on";
+
+    private final String FILTER_MESSAGE = "Anti spam measurements are now: ";
+
     private boolean filterIsOn = false;
+
+    public UserFilteringAdapter(){
+        //setup of basic Filters needed
+        userIDs = new HashMap<String, Boolean>();
+        bannedWords = new HashSet<String>();
+
+        userIDs.put("431542303346262039", false); //scholfz
+        userIDs.put("301485597925703683", false); //Ayresia
+
+        bannedWords.addAll(Arrays.asList("opop", "xd", "lmao", "lol", "rofl", "eksd", "dx", "oga"));
+    }
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 
         try {
-            if (event.getMember().getRoles().contains(event.getGuild().getRolesByName("Moderator", true).get(0))) {
-
-                if (event.getMessage().getContentRaw().equalsIgnoreCase(">scholzfilter on")) {
+            if (!event.getGuild().getRolesByName("Moderator", true).isEmpty()) {
+                if (event.getMessage().getContentRaw().equalsIgnoreCase(FILTER_ON)) {
                     filterIsOn = true;
-                    event.getChannel().sendMessage("Anti scholz measures activated").queue();
-                } else if (event.getMessage().getContentRaw().equalsIgnoreCase(">scholzfilter off")) {
+                } else if (event.getMessage().getContentRaw().equalsIgnoreCase(FILTER_OFF)) {
                     filterIsOn = false;
-                    event.getChannel().sendMessage("Anti scholz measures deactivated").queue();
                 }
-
+                event.getChannel().sendMessage(FILTER_MESSAGE + isSpamFilterActiveAsString()).queue();
             }
         } catch (NullPointerException e) {
             // user has no roles
         }
 
-        if (event.getAuthor().getId().equalsIgnoreCase(scholzID) && filterIsOn) {
-
+        if (userIDs.containsKey(event.getAuthor().getId()) && filterIsOn) {
             for (String s : bannedWords) {
                 if (event.getMessage().getContentRaw().toLowerCase().contains(s)) {
                     event.getMessage().delete().queue();
                     break;
                 }
-
             }
-
         }
+    }
 
+    public String isSpamFilterActiveAsString(){
+        return filterIsOn ? "activated" : "deactivate";
     }
 }
