75d67599d0615bd3feb6a3f0a03a4a6d53a4113c
Doppey <25537078+Doppeey@users.noreply.github.com>
2019-05-07 21:08:48 +0200

    lil bit of cleanup :)

--- src/main/java/EventsAndCommands/FunCommands/DeletThisCommand.java
+++ src/main/java/EventsAndCommands/FunCommands/DeletThisCommand.java
@@ -5,36 +5,27 @@ import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
 import java.io.File;
-import java.io.InputStream;
 import java.util.Random;
 
 public class DeletThisCommand extends Command {
 
-    private InputStream is;
-    private ClassLoader classloader;
-
-    public DeletThisCommand(){
-        this.name="delet";
+    public DeletThisCommand() {
+        this.name = "delet";
         this.category = Categories.Fun;
         this.help = "Sends random delet this picture";
     }
-
 
     private File file;
 
     @Override
     protected void execute(CommandEvent commandEvent) {
 
-
         try {
-            String fileName = "delet/"+ (new Random().nextInt(21)+1)+".jpg";
-
-           file = new File(fileName);
-
+            String fileName = "delet/" + (new Random().nextInt(21) + 1) + ".jpg";
+            file = new File(fileName);
+                        
         } catch (Exception e) {
             e.printStackTrace();
-
-;
             return;
         }
 
