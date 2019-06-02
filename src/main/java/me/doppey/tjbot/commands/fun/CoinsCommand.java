342caa6239b129aceb1ad08edc5989a0a7743783
Doppey <25537078+Doppeey@users.noreply.github.com>
2019-06-02 22:51:04 +0200

    coin feature

---
+++ src/main/java/me/doppey/tjbot/commands/fun/CoinsCommand.java
@@ -0,0 +1,57 @@
+package me.doppey.tjbot.commands.fun;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import me.doppey.tjbot.Categories;
+import org.bson.Document;
+
+public class CoinsCommand extends Command {
+
+    MongoCollection javacoinsDb;
+
+    public CoinsCommand(MongoDatabase db) {
+        this.name = "coins";
+        this.help =  "Together Javas \"Javacoins\". Use `>Coins help`";
+        this.category = Categories.Fun;
+        this.javacoinsDb = db.getCollection("javacoins");
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+
+        String userId = event.getAuthor().getId();
+
+        //Only works on TJ
+        if(!event.getGuild().getId().equalsIgnoreCase("272761734820003841")){
+            event.reply("This only works on together java");
+            return;
+        }
+
+        if(event.getArgs().isEmpty()){
+
+
+            final FindIterable userIterable = javacoinsDb.find(new Document("userId", userId));
+
+            if(userIterable.first() != null){
+
+                if(!event.getChannel().getName().toLowerCase().contains("bots")){
+                    event.getMessage().delete().queue();
+                    event.getGuild().getTextChannelsByName("bots",true).get(0).sendMessage(event.getMember().getAsMention()+" You have "+"`"+ ((Document)userIterable.first()).getInteger("javacoins")+"` javacoins.").queue();
+                    return;
+                }
+
+                event.reply("You have "+"`"+ ((Document)userIterable.first()).getInteger("javacoins")+"` javacoins.");
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ src/main/java/me/doppey/tjbot/events/fun/JavacoinEvent.java
@@ -0,0 +1,76 @@
+package me.doppey.tjbot.events.fun;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import me.doppey.tjbot.InoriChan;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import org.bson.Document;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public class JavacoinEvent extends ListenerAdapter {
+
+    MongoCollection javacoinCollection;
+
+    public JavacoinEvent(MongoDatabase db) {
+        javacoinCollection = db.getCollection("javacoins");
+    }
+
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+
+        String channelName = event.getChannel().getName().toLowerCase();
+
+        //Don't count messages in testing servers
+        if(!event.getGuild().getId().equalsIgnoreCase("272761734820003841")){
+            return;
+        }
+
+        //Don't count messages in these channels
+        if (channelName.contains("help") || channelName.contains("spam") || channelName.contains("bot")) {
+            return;
+        }
+
+        //If the user does not yet exist in the database
+        final String userId = event.getAuthor().getId();
+        if (javacoinCollection.find(new Document("userId", userId)).first() == null) {
+            Document doc = new Document()
+                    .append("userId", userId)
+                    .append("javacoins", 50)
+                    .append("dailyTime", LocalDateTime.now().toInstant(ZoneOffset.UTC))
+                    .append("lastMessageTime", LocalDateTime.now().toInstant(ZoneOffset.UTC));
+
+            InoriChan.LOGGER.info("User "+event.getMember().getEffectiveName()+" has received their 25 daily javacoins!");
+            javacoinCollection.insertOne(doc);
+            return;
+        }
+
+
+        // If the name is found in the database, check if its been 24h since last message (if yes they get 50 coins for first message of the day)
+
+        Document searchDoc = new Document().append("userId", userId);
+        Document userDoc = (Document) javacoinCollection.find(searchDoc).first();
+        LocalDateTime userDateTime = LocalDateTime.from(((Date) userDoc.get("dailyTime")).toInstant().atZone(ZoneOffset.UTC));
+        LocalDateTime userLastMessageTime = LocalDateTime.from(((Date) userDoc.get("lastMessageTime")).toInstant().atZone(ZoneOffset.UTC));
+
+        //Check for daily
+        if (ChronoUnit.DAYS.between(userDateTime, LocalDateTime.now().atZone(ZoneOffset.UTC)) >= 1) {
+            javacoinCollection.updateOne(searchDoc, new Document("$set", new Document("dailyTime", LocalDateTime.now().toInstant(ZoneOffset.UTC))));
+            javacoinCollection.updateOne(searchDoc, new Document("$set", new Document("javacoins", userDoc.getInteger("javacoins") + 25)));
+            InoriChan.LOGGER.info("User "+event.getMember().getEffectiveName()+" has received their 50 daily javacoins!");
+            return;
+        }
+
+        //Check for cooldown
+        if (ChronoUnit.MINUTES.between(userLastMessageTime, LocalDateTime.now().atZone(ZoneOffset.UTC)) >= 1) {
+            javacoinCollection.updateOne(searchDoc, new Document("$set", new Document("javacoins", userDoc.getInteger("javacoins") + 1)));
+            javacoinCollection.updateOne(searchDoc, new Document("$set", new Document("lastMessageTime", LocalDateTime.now().toInstant(ZoneOffset.UTC))));
+            return;
+        }
+
+    }
+}
